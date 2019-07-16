8aaed7b678b753c46a69e8d6d7bf7fadd13e2088
sialvsic <sialvsic@outlook.com>
2019-07-16 22:45:43 +0800

    commint temp

---
+++ Drag/index.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+
+};
+
+function dragStart() {
+  console.log('dragStart');
+}
+
+function drag() {
+  console.log('drag');
+}
+
+function sideDrag() {
+  console.log('sideDrag');
+}
+
+function dragOver() {
+  console.log('dragOver');
+}
+
+function sideDragOver() {
+  console.log('sideDragOver');
+}
+
+function dragEnter() {
+  console.log('dragEnter');
+}
+
+function sideDragEnter() {
+  console.log('sideDragEnter');
+}
+
+function dragLeave() {
+  console.log('dragLeave');
+}
+
+function sideDragLeave() {
+  console.log('sideDragLeave');
+}
+
+function dragExit() {
+  console.log('dragExit');
+}
+
+function dragEnd() {
+  console.log('dragEnd');
+}
+
+function mouseOver() {
+  console.log('mouseOver');
+}
